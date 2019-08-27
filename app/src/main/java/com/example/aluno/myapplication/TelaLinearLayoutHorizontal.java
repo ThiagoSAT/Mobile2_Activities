3638e0cd14834efd0114129c11e889e5fa3c6cbd
ThiagoSAT <thiago.avfc1874@gmail.com>
2019-08-27 12:33:41 -0300

    Aula Activities

---
+++ app/src/main/java/com/example/aluno/myapplication/TelaLinearLayoutHorizontal.java
@@ -0,0 +1,12 @@
+package com.example.aluno.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaLinearLayoutHorizontal extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_linear_horizontal);
+    }
+}
